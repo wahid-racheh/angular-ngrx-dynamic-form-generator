134d31174f5a4a0709bdb475115a1b7e510268fc
racheh <racheh.wahid@gmail.com>
2020-03-01 03:28:27 +0100

    chore(navigation): add form page navigation

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,10 @@ import { Component, Input } from '@angular/core';
 })
 export class NavbarComponent {
   @Input() public title: string;
+
+  constructor(private router: Router) {}
+
+  public go(): void {
+    this.router.navigate(['/demo/forms'])
+  }
 }
