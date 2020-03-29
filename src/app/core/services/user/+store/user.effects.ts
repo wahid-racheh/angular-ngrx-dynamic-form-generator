eb45f607cba4ca2c1e527206a32747efd1dbbd88
racheh.wahid <racheh.wahid@cn.medecin.fr>
2020-03-29 16:54:09 +0200

    refactor(errors): add mock http debounce time & handle mock http error

--- src/app/core/services/user/+store/user.effects.ts
+++ src/app/core/services/user/+store/user.effects.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { asyncScheduler, EMPTY as empty, of } from 'rxjs';
 import {
@@ -11,22 +12,32 @@ import {
   takeUntil
 } from 'rxjs/operators';
 
+import { ErrorFacade } from '@app/core/interceptors/error/+store/error.facade';
 import * as UserActions from '@app/core/services/user/+store/user.actions';
 import { User } from '@app/core/services/user/models/user.interface';
 import { UserService } from '@app/core/services/user/services/user.service';
 
 @Injectable()
 export class UserEffects {
-  constructor(private actions$: Actions, private userService: UserService) {}
+  constructor(
+    private actions$: Actions,
+    private userService: UserService,
+    private errorFacade: ErrorFacade
+  ) {}
 
-  public getUser$ = createEffect(() =>
+  public getUser$ = createEffect(() => ({ scheduler = asyncScheduler } = {}) =>
     this.actions$.pipe(
       ofType(UserActions.getUser),
+      debounceTime(environment.mockHttpDebounceTime, scheduler),
       map(action => action),
       concatMap(action =>
         this.userService.getById(action.id).pipe(
           map((response: User) => UserActions.getUserSuccess({ response })),
-          catchError(error => of(UserActions.getUserFail(error)))
+          catchError((error: any) => {
+            this.errorFacade.throwCustomError(error);
+            this.errorFacade.showErrorPage();
+            return of(UserActions.getUserFail(error));
+          })
         )
       )
     )
@@ -45,9 +56,13 @@ export class UserEffects {
 
         return this.userService.search().pipe(
           takeUntil(nextSearch$),
-          map((data: User[]) => data.filter(u => u.name.toLowerCase().indexOf(name.toLowerCase()) > -1)),
+          map((data: User[]) => data.filter(u => u.name.toLowerCase().indexOf(name) > -1)),
           map((users: User[]) => UserActions.searchSuccess({ response: users })),
-          catchError((error: any) => of(UserActions.searchFail({ error })))
+          catchError((error: any) => {
+            this.errorFacade.throwCustomError(error);
+            this.errorFacade.showErrorPage();
+            return of(UserActions.searchFail({ error }));
+          })
         );
       })
     )
